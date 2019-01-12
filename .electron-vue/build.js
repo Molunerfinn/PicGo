86265ee5c5a47480bf7e0e71ebd3166ea6ad713f
Molunerfinn <marksz@teamsz.xyz>
2019-01-12 12:48:54 +0800

    Updated: build version

--- .electron-vue/build.js
+++ .electron-vue/build.js
@@ -12,7 +12,7 @@ const Multispinner = require('multispinner')
 
 const mainConfig = require('./webpack.main.config')
 const rendererConfig = require('./webpack.renderer.config')
-const webConfig = require('./webpack.web.config')
+// const webConfig = require('./webpack.web.config')
 
 const doneLog = chalk.bgGreen.white(' DONE ') + ' '
 const errorLog = chalk.bgRed.white(' ERROR ') + ' '
@@ -98,20 +98,20 @@ function pack (config) {
   })
 }
 
-function web () {
-  del.sync(['dist/web/*', '!.gitkeep'])
-  webConfig.mode = 'production'
-  webpack(webConfig, (err, stats) => {
-    if (err || stats.hasErrors()) console.log(err)
+// function web () {
+//   del.sync(['dist/web/*', '!.gitkeep'])
+//   webConfig.mode = 'production'
+//   webpack(webConfig, (err, stats) => {
+//     if (err || stats.hasErrors()) console.log(err)
 
-    console.log(stats.toString({
-      chunks: false,
-      colors: true
-    }))
+//     console.log(stats.toString({
+//       chunks: false,
+//       colors: true
+//     }))
 
-    process.exit()
-  })
-}
+//     process.exit()
+//   })
+// }
 
 function greeting () {
   const cols = process.stdout.columns
