7f8c6fd60e7c9ab6abf4fc60d412cc1ab7bf58c4
Molunerfinn <marksz@teamsz.xyz>
2019-03-11 13:40:39 +0800

    Fixed: #195 || windows7's bug of uploading image in clipboard

--- static/windows.ps1
+++ static/windows.ps1
@@ -1,31 +1,39 @@
-
 param($imagePath)
 
 # Adapted from https://github.com/octan3/img-clipboard-dump/blob/master/dump-clipboard-png.ps1
 
 Add-Type -Assembly PresentationCore
-$file = Get-Clipboard -Format FileDropList
-if ($file -ne $null) {
-    Convert-Path $file
-    Exit 1
+function main {
+    $img = [Windows.Clipboard]::GetImage()
+
+    if ($img -eq $null) {
+        "no image"
+        Exit 1
+    }
+
+    if (-not $imagePath) {
+        "no image"
+        Exit 1
+    }
+
+    $fcb = new-object Windows.Media.Imaging.FormatConvertedBitmap($img, [Windows.Media.PixelFormats]::Rgb24, $null, 0)
+    $stream = [IO.File]::Open($imagePath, "OpenOrCreate")
+    $encoder = New-Object Windows.Media.Imaging.PngBitmapEncoder
+    $encoder.Frames.Add([Windows.Media.Imaging.BitmapFrame]::Create($fcb)) | out-null
+    $encoder.Save($stream) | out-null
+    $stream.Dispose() | out-null
+
+    $imagePath
 }
-$img = [Windows.Clipboard]::GetImage()
 
-if ($img -eq $null) {
-    "no image"
-    Exit 1
+try {
+  $file = Get-Clipboard -Format FileDropList
+  if ($file -ne $null) {
+      Convert-Path $file
+      Exit 1
+  }
+} catch {
+  main
 }
 
-if (-not $imagePath) {
-    "no image"
-    Exit 1
-}
-
-$fcb = new-object Windows.Media.Imaging.FormatConvertedBitmap($img, [Windows.Media.PixelFormats]::Rgb24, $null, 0)
-$stream = [IO.File]::Open($imagePath, "OpenOrCreate")
-$encoder = New-Object Windows.Media.Imaging.PngBitmapEncoder
-$encoder.Frames.Add([Windows.Media.Imaging.BitmapFrame]::Create($fcb)) | out-null
-$encoder.Save($stream) | out-null
-$stream.Dispose() | out-null
-
-$imagePath
+main
