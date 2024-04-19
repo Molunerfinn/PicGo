8e91582adc598ee6d233da9110ff018e0c1623cb
PiEgg <PiEgg@moego.pet>
2024-04-19 09:39:44 +0800

    :bug: Fix: config page scroll bug
    ISSUES CLOSED: #1237

--- src/renderer/pages/UploaderConfigPage.vue
+++ src/renderer/pages/UploaderConfigPage.vue
@@ -1,5 +1,8 @@
 <template>
-  <div id="config-list-view">
+  <div
+    id="config-list-view"
+    class="h-[425px]"
+  >
     <div class="view-title">
       {{ $T('SETTINGS') }}
     </div>
@@ -239,9 +242,10 @@ export default {
     .selected
       border 1px solid #409EFF
   .set-default-container
-    position absolute
+    position fixed
     bottom 20px
     width 100%
     .set-default-btn
       width 250px
+      transform translateX(-50%)
 </style>
