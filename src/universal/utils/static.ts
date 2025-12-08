9d4ea8277df03d014a66ac85bdc73e4f916ed93a
PiEgg <marksz@teamsz.xyz>
2025-12-08 21:27:03 +0800

    :package: Chore: change release url

--- src/universal/utils/static.ts
+++ src/universal/utils/static.ts
@@ -1,6 +1,6 @@
 export const CLIPBOARD_IMAGE_FOLDER = 'picgo-clipboard-images'
 export const FORM_IMAGE_FOLDER = 'picgo-form-images'
 export const RELEASE_URL = 'https://api.github.com/repos/Molunerfinn/PicGo/releases'
-export const RELEASE_URL_BACKUP = 'https://picgo-release.molunerfinn.com'
+export const RELEASE_URL_BACKUP = 'https://release.picgo.app'
 export const STABLE_RELEASE_URL = 'https://github.com/Molunerfinn/PicGo/releases/latest'
 export const BETA_RELEASE_URL = 'https://github.com/Molunerfinn/PicGo/releases'
